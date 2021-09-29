7e3777c87b0fb524e51270026d54506b8027654d
Jason Cochard <cochard@protonmail.com>
2021-09-28 22:38:09 -0700

    Added a ResetOnAbort flag which, when set, will cause an arm.SystemReset() upon abort() -- called by runtime._panic() and runtime.runtimePanic()

--- src/runtime/runtime_cortexm_abort.go
+++ src/runtime/runtime_cortexm_abort.go
@@ -6,7 +6,13 @@ import (
 	"device/arm"
 )
 
+var ResetOnAbort bool
+
 func abort() {
+	// optionally cause a system reset upon runtime._panic() or runtime.runtimePanic()
+	if ResetOnAbort {
+		arm.SystemReset()
+	}
 	// lock up forever
 	for {
 		arm.Asm("wfi")
